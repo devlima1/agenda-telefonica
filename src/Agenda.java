a6634d5297dc93bf1a6ecc0a584a2a00ede72e92
devlima1 <04lima.gabriel@gmail.com>
2025-04-25 10:56:43 -0300

    feat: implementar classe Agenda com métodos de CRUD para contatos

---
+++ src/Agenda.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Agenda {
+    private ArrayList<Contato> contatos;
+
+    public Agenda() {
+        contatos = new ArrayList<>();
+    }
+
+    public void adicionarContato(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public boolean removerContato(String nome) {
+        for (Contato c : contatos) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                contatos.remove(c);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Contato buscarContato(String nome) {
+        for (Contato c : contatos) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    
+    public void listarContatos() {
+        if (contatos.isEmpty()) {
+            System.out.println("Agenda vazia.");
+        } else {
+            for (Contato c : contatos) {
+                System.out.println(c);
+            }
+        }
+    }
+
+    public boolean editarContato(String nome, Contato novoContato) {
+        for (int i = 0; i < contatos.size(); i++) {
+            if(contatos.get(i).getNome().equalsIgnoreCase(nome)) {
+                contatos.set(i, novoContato);
+                return true;
+            }
+        }
+        return false;
+    }
+}
